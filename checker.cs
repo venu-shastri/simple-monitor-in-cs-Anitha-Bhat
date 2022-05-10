db16775f76d0191befc94208ea1441e98895a2cb
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-10 14:44:56 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -48,56 +48,6 @@ class Checker
 
   static int Main()
   {
-   using System;
-using System.Diagnostics;
-
-class Checker
-{
-  static bool batteryIsOk(float temperature, float stateOfCharge, float chargeRate)
-  {
-    return isTemperatureInRange(temperature) && isStateOfChargeInRange(stateOfCharge) && isChargeRateInRange(chargeRate);
-   
-  }
-
-  private static bool isTemperatureInRange(float temperature)
-  {
-    if (temperature < 0 || temperature > 45)
-    {
-      printText("Temperature is out of range!");
-      return false;
-    }
-
-    return true;
-  }
-
-  private static bool isStateOfChargeInRange(float stateOfCahrge)
-  {
-    if (stateOfCahrge < 20 || stateOfCahrge > 80)
-    {
-      printText("State of Charge is out of range!");
-      return false;
-    }
-    return true;
-  }
-
-  private static bool isChargeRateInRange(float chargeRate)
-  {
-    if (chargeRate > 0.8)
-    {
-      printText("Charge Rate is out of range!");
-      return false;
-    }
-
-    return true;
-  }
-
-  private static void printText(string text)
-  {
-    Console.WriteLine(text);
-  }
-
-  static int Main()
-  {
 
 
     Debug.Assert(batteryIsOk(25, 70, 0.7f));
